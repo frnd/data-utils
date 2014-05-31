cbf6bd54dfac05c5067269ca31469d196736917d
Fernando Gonzalez <fernando.gonzalez.dediego@gmail.com>
2014-05-31 03:09:30 +0200

    refactoring incorrect name.

--- src/main/java/es/frnd/matrix/additive/Accumulator.java
+++ src/main/java/es/frnd/matrix/additive/Accumulator.java
@@ -2,11 +2,11 @@ package es.frnd.matrix.additive;
 
 import es.frnd.matrix.Matrix;
 
-public abstract class Accummulator<T, V> implements Matrix.Accumulator<T, V> {
+public abstract class Accumulator<T, V> implements Matrix.Accumulator<T, V> {
 
 	PropertyResolver<T, V> propertyResolver;
 	
-	public Accummulator(PropertyResolver<T, V> propertyResolver) {
+	public Accumulator(PropertyResolver<T, V> propertyResolver) {
 		this.propertyResolver = propertyResolver;
 	}
 	
--- src/main/java/es/frnd/matrix/generic/GenericMatrix.java
+++ src/main/java/es/frnd/matrix/generic/GenericMatrix.java
@@ -1,104 +1,104 @@
 package es.frnd.matrix.generic;
 
+import es.frnd.matrix.AbstractMatrix;
+import es.frnd.matrix.Matrix;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import es.frnd.matrix.AbstractMatrix;
-import es.frnd.matrix.Matrix;
-
 /**
  * Sorted by the natural sorting of the row and column headers.
- * 
- * @author Fernando Gonzalez
+ *
  * @param <T>
  * @param <V>
  * @param <R>
  * @param <C>
+ * @author Fernando Gonzalez
  */
 public class GenericMatrix<R, C, T, V> extends AbstractMatrix<R, C, T, V> implements
-		Matrix<R, C, T, V> {
+        Matrix<R, C, T, V> {
 
-	private final Resolver<T, R>		rowResolver;
-	private final Resolver<T, C>		colResolver;
-	private final Accumulator<T, V> accummulator;
+    private final Resolver<T, R> rowResolver;
+    private final Resolver<T, C> colResolver;
+    private final Accumulator<T, V> accumulator;
 
-	public GenericMatrix(Resolver<T, R> rowResolver, Resolver<T, C> colResolver,
-			Accumulator<T, V> accummulator) {
-		super(new HashMap<R, Map<C, Matrix.Cell<T, V>>>(), new ArrayList<T>(), new HashMap<C, Matrix.Cell<T, V>>(),
-				new HashMap<R, Matrix.Cell<T, V>>(), new Cell<T, V>(accummulator));
-		this.rowResolver = rowResolver;
-		this.colResolver = colResolver;
-		this.accummulator = accummulator;
-	}
+    public GenericMatrix(Resolver<T, R> rowResolver, Resolver<T, C> colResolver,
+                         Accumulator<T, V> accumulator) {
+        super(new HashMap<R, Map<C, Matrix.Cell<T, V>>>(), new ArrayList<T>(), new HashMap<C, Matrix.Cell<T, V>>(),
+                new HashMap<R, Matrix.Cell<T, V>>(), new Cell<T, V>(accumulator));
+        this.rowResolver = rowResolver;
+        this.colResolver = colResolver;
+        this.accumulator = accumulator;
+    }
 
-	static class Cell<T, V> implements Matrix.Cell<T, V> {
+    static class Cell<T, V> implements Matrix.Cell<T, V> {
 
-		private final Accumulator<T, V> accummulator;
-		private V							value;
-		private List<T>						items;
+        private final Accumulator<T, V> accumulator;
+        private V value;
+        private List<T> items;
 
-		public Cell(Accumulator<T, V> accummulator) {
-			this.accummulator = accummulator;
-			items = new ArrayList<T>();
-		}
+        public Cell(Accumulator<T, V> accumulator) {
+            this.accumulator = accumulator;
+            items = new ArrayList<T>();
+        }
 
-		public void add(T item) {
-			items.add(item);
-			accumulate();
-		}
+        public void add(T item) {
+            items.add(item);
+            accumulate();
+        }
 
-		/*
-		 * This method is for internal use only. Will add an item without
-		 * calling to Cell.accumulate()
-		 */
-		void addInternal(T item) {
-			items.add(item);
-		}
+        /*
+         * This method is for internal use only. Will add an item without
+         * calling to Cell.accumulate()
+         */
+        void addInternal(T item) {
+            items.add(item);
+        }
 
-		public void accumulate() {
-			value = accummulator.accumulate(items);
-		}
+        public void accumulate() {
+            value = accumulator.accumulate(items);
+        }
 
-		/**
-		 * @return the value
-		 */
-		public V getValue() {
-			return value;
-		}
+        /**
+         * @return the value
+         */
+        public V getValue() {
+            return value;
+        }
 
-		/**
-		 * @return the items
-		 */
-		public List<T> getItems() {
-			return items;
-		}
+        /**
+         * @return the items
+         */
+        public List<T> getItems() {
+            return items;
+        }
 
-		@Override
-		public void clear() {
-			items.clear();
-			value = null;
-		}
-	}
+        @Override
+        public void clear() {
+            items.clear();
+            value = null;
+        }
+    }
 
-	@Override
-	protected es.frnd.matrix.Matrix.Resolver<T, R> getRowResolver() {
-		return rowResolver;
-	}
+    @Override
+    protected es.frnd.matrix.Matrix.Resolver<T, R> getRowResolver() {
+        return rowResolver;
+    }
 
-	@Override
-	protected es.frnd.matrix.Matrix.Resolver<T, C> getColResolver() {
-		return colResolver;
-	}
+    @Override
+    protected es.frnd.matrix.Matrix.Resolver<T, C> getColResolver() {
+        return colResolver;
+    }
 
-	@Override
-	protected Cell<T, V> createCell() {
-		return new Cell<T, V>(accummulator);
-	}
+    @Override
+    protected Cell<T, V> createCell() {
+        return new Cell<T, V>(accumulator);
+    }
 
-	@Override
-	protected Map<C, Matrix.Cell<T, V>> createBackingMap() {
-		return new HashMap<C, Matrix.Cell<T, V>>();
-	}
+    @Override
+    protected Map<C, Matrix.Cell<T, V>> createBackingMap() {
+        return new HashMap<C, Matrix.Cell<T, V>>();
+    }
 }
